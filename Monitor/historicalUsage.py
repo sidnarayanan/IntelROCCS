61af3d8ded21d3fd24eebc08475eb57592edfcd1
Sid Narayanan <sidn@mit.edu>
2016-04-08 14:42:45 +0200

    i forget

--- Monitor/historicalUsage.py
+++ Monitor/historicalUsage.py
@@ -28,6 +28,22 @@ def nSites(ds,utime):
         r += 1
   return r
 
+def nDeleted(ds,utime,interval):
+  r = 0
+  for k,v in ds.movement.iteritems():
+    for vv in v[1]:
+      if utime-interval < vv < utime:
+        r += 1
+  return r
+
+def nReplicated(ds,utime,interval):
+  r = 0
+  for k,v in ds.movement.iteritems():
+    for vv in v[0]:
+      if utime-interval < vv < utime:
+        r += 1
+  return r
+
 def dictToSortedArr(d):
   l = []
   for k,v in sorted(d.iteritems()):
@@ -46,6 +62,8 @@ now = time()-2*sPerD
 lastCopies = {}
 lastCopyVol = {}
 totalVol = {}
+volDeleted = {}
+volReplicated = {}
 
 #split by data tier
 nAccesses = {}
@@ -57,7 +75,7 @@ nDatasets = {}
 times = []
 for iM in xrange(nMonths,-1,-1):
   times.append(now-iM*sPerM)
-  for d in [lastCopies,lastCopyVol,totalVol]:
+  for d in [lastCopies,lastCopyVol,totalVol,volDeleted,volReplicated]:
     d[now-iM*sPerM]=0
 for d in [nAccesses,nFiles,sizeGB,nReplicas,nDatasets]:
   for tier in ['AOD','AODSIM','MINIAOD','MINIAODSIM']:
@@ -71,6 +89,8 @@ for utime in times:
     if n>0:   
       # it was part of the pool at this time
       totalVol[utime] += dobj.sizeGB*n
+      volDeleted[utime] += nDeleted(dobj,utime,sPerM)*dobj.sizeGB
+      volReplicated[utime] += nReplicated(dobj,utime,sPerM)*dobj.sizeGB
       if n==1:
         lastCopies[utime] += 1
         lastCopyVol[utime] += dobj.sizeGB
@@ -95,11 +115,11 @@ for k,v in sorted(lastCopies.iteritems()):
   dates.append(datetime.fromtimestamp(int(k)))
   lc.append(lastCopyVol[k]*1./totalVol[k])
 
-seaborn.set_style("whitegrid")
+seaborn.set_style("ticks")
 seaborn.despine()
 # seaborn.despine(left=True)
 
-plt.plot_date(dates,lc,c=seaborn.xkcd_rgb['medium green'], ls='-', lw=2, marker='')
+plt.plot_date(dates,lc,c=seaborn.xkcd_rgb['medium green'], ls='-', lw=3, marker='')
 plt.axes().set_ylim(ymin=0,ymax=0.6)
 plt.xlabel('Time',fontsize=24)
 plt.ylabel('Last copy fraction',fontsize=24)
@@ -113,12 +133,12 @@ for tier in ['AOD','AODSIM','MINIAOD','MINIAODSIM']:
   sizes = dictToSortedArr(sizeGB[tier])
   color = seaborn.xkcd_rgb['pale red'] if 'MINI' in tier else seaborn.xkcd_rgb['denim blue']
   style = '-' if 'SIM' in tier else '--'
-  plt.plot_date(dates,na/sizes,c=color, ls=style, lw=2,label=tier, marker='')
+  plt.plot_date(dates,na/sizes,c=color, ls=style, lw=3,label=tier, marker='')
 
 plt.axes().set_ylim(ymin=0,ymax=20)
 plt.xlabel('Time',fontsize=24)
 plt.ylabel('Accesses per GB per month',fontsize=24)
-plt.legend()
+plt.legend(fontsize=24)
 saveFig('popgb')
 
 plt.clf()
@@ -128,12 +148,12 @@ for tier in ['AOD','AODSIM','MINIAOD','MINIAODSIM']:
   files = dictToSortedArr(nFiles[tier])
   color = seaborn.xkcd_rgb['pale red'] if 'MINI' in tier else seaborn.xkcd_rgb['denim blue']
   style = '-' if 'SIM' in tier else '--'
-  plt.plot_date(dates,na/files,c=color, ls=style, lw=2,label=tier, marker='')
+  plt.plot_date(dates,na/files,c=color, ls=style, lw=3,label=tier, marker='')
 
 plt.axes().set_ylim(ymin=0,ymax=10)
 plt.xlabel('Time',fontsize=24)
 plt.ylabel('Accesses per file per month',fontsize=24)
-plt.legend()
+plt.legend(fontsize=24)
 saveFig('popfile')
 
 plt.clf()
@@ -144,13 +164,32 @@ for tier in ['AOD','AODSIM','MINIAOD','MINIAODSIM']:
   ndata = dictToSortedArr(nDatasets[tier])
   color = seaborn.xkcd_rgb['pale red'] if 'MINI' in tier else seaborn.xkcd_rgb['denim blue']
   style = '-' if 'SIM' in tier else '--'
-  plt.plot_date(dates,nrepl/ndata,c=color, ls=style, lw=2,label=tier, marker='')
+  plt.plot_date(dates,nrepl/ndata,c=color, ls=style, lw=3,label=tier, marker='')
 
 
 plt.axes().set_ylim(ymin=0,ymax=5)
 plt.xlabel('Time',fontsize=24)
 plt.ylabel('Average number of replicas',fontsize=24)
-plt.legend()
+plt.legend(fontsize=24)
 saveFig('replicas')
+
+plt.clf()
+
+
+for tier in ['AOD','AODSIM','MINIAOD','MINIAODSIM']:
+  nrepl = dictToSortedArr(nReplicas[tier])
+  ndata = dictToSortedArr(nDatasets[tier])
+  color = seaborn.xkcd_rgb['pale red'] if 'MINI' in tier else seaborn.xkcd_rgb['denim blue']
+  style = '-' if 'SIM' in tier else '--'
+
+plt.plot_date(dates,dictToSortedArr(volDeleted)/1000000.,c=seaborn.xkcd_rgb['orangey red'], ls='-', lw=3,label='Deletions', marker='')
+plt.plot_date(dates,dictToSortedArr(volReplicated)/1000000.,c=seaborn.xkcd_rgb['bluey green'], ls='-', lw=3,label='Replications', marker='')
+
+
+plt.axes().set_ylim(ymin=0,ymax=15)
+plt.xlabel('Time',fontsize=24)
+plt.ylabel('Volume [PB/month]',fontsize=24)
+plt.legend(fontsize=24)
+saveFig('movement')
 
 plt.clf()
